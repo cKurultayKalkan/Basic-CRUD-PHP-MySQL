ccaaed6e06d9e56819a1127ec5913b83be94b923
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2022-05-24 03:35:08 +0300

    get role from endpoint

--- credentials.php
+++ credentials.php
@@ -12,23 +12,22 @@ if (curl_error($get_token_ch)) {
     print curl_error($get_token_ch);
 }
 
-$get_user_role_name = curl_init("http://169.254.169.254/latest/meta-data/iam/info");
+$get_user_role_name = curl_init("http://169.254.169.254/latest/meta-data/iam/security-credentials/");
 curl_setopt($get_user_role_name, CURLOPT_RETURNTRANSFER, true);
 $token_header = $header = "X-aws-ec2-metadata-token: " . $token;
 curl_setopt($get_user_role_name, CURLOPT_HTTPHEADER, array($token_header));
 $role_name = curl_exec($get_user_role_name);
-curl_close($get_user_role_name);
-$role_name_parsed = json_decode($role_name);
 if (curl_error($get_user_role_name)) {
     print_r("Role Error");
     print curl_error($get_user_role_name);
 } else {
-    $splitted = explode("/", $role_name_parsed->InstanceProfileArn);
-    $target_role_name = $splitted[1];
+    $target_role_name = $role_name;
 }
+curl_close($get_user_role_name);
 
 
-$get_region = curl_init("http://169.254.169.254/latest/meta-data/hostname");
+$url = "http://169.254.169.254/latest/meta-data/hostname";
+$get_region = curl_init($url);
 curl_setopt($get_region, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($get_region, CURLOPT_HTTPHEADER, array($token_header));
 $region_raw = curl_exec($get_region);
@@ -39,10 +38,12 @@ if (curl_error($get_region)) {
 } else {
     $b = explode(".", $region_raw);
     $aws_region = $b[1];
-    print_r('AWS Region:'. $aws_region);
+    print_r('AWS Region:' . $aws_region);
 }
 
-$get_credentials = curl_init("http://169.254.169.254/latest/meta-data/iam/security-credentials/" . $target_role_name);
+$url = "http://169.254.169.254/latest/meta-data/iam/security-credentials/" . $target_role_name;
+
+$get_credentials = curl_init($url);
 curl_setopt($get_credentials, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($get_credentials, CURLOPT_HTTPHEADER, array($token_header));
 $credentials_raw = curl_exec($get_credentials);
